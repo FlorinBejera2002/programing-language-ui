4b91b4633289d3e0bbbd1e2a54dd6bd1e3bfe506
FlorinBejera2002 <florinpetru0306@gmail.com>
2024-09-09 00:26:01 +0300

    fix: fix error whit routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import ReactDOM from 'react-dom/client'
 import React from 'react'
-
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import { App } from './App.tsx'
+import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
